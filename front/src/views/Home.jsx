fa546ab2b539fbb42101635fa88bac4b2dc9cad2
armql <am57839@ubt-uni.net>
2023-09-27 22:53:40 +0200

    dynamic display of schedules

--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -1,39 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Home() {
+    const [schedule, setSchedule] = useState(false);
+
+    const toggleScheduler = () => {
+        setSchedule(!schedule);
+    }
     return (
         <div className='parent'>
             <div className='bg-sky-950 text-white p-12 flex items-center justify-center font-bold text-3xl'>
-                Our Schedules
+                Orari i termineve
             </div>
             <div className='grid bg-white grid-cols-3 lg:grid-cols-6 items-center justify-center text-center'>
-                <div className='relative group p-10 border bg-white hover:border-sky-300 hover:shadow transition cursor-pointer duration-500 active:translate-y-1'>
+                <button onClick={toggleScheduler} type="button" className='relative group text-xl p-10 border bg-white hover:border-sky-300 hover:shadow transition cursor-pointer active:bg-sky-50'>
                     Monday
-                </div>
-                <div className='group p-10 border bg-white hover:border-sky-300 hover:shadow transition cursor-pointer duration-500 active:translate-y-1'>
+                </button>
+                <button type="button" className='group p-10 border bg-white hover:border-sky-300 hover:shadow text-xl transition cursor-pointer active:bg-sky-50'>
                     Tuesday
-                </div>
-                <div className='group p-10 border bg-white hover:border-sky-300 hover:shadow transition cursor-pointer duration-500 active:translate-y-1'>
+                </button>
+                <button type="button" className='group p-10 border bg-white hover:border-sky-300 hover:shadow text-xl transition cursor-pointer active:bg-sky-50'>
                     Wednesday
-                </div>
-                <div className='group p-10 border bg-white hover:border-sky-300 hover:shadow transition cursor-pointer duration-500 active:translate-y-1'>
+                </button>
+                <button type="button" className='group p-10 border bg-white hover:border-sky-300 hover:shadow text-xl transition cursor-pointer active:bg-sky-50'>
                     Thursday
-                </div>
-                <div className='group p-10 border bg-white hover:border-sky-300 hover:shadow transition cursor-pointer duration-500 active:translate-y-1'>
+                </button>
+                <button type="button" className='group p-10 border bg-white hover:border-sky-300 hover:shadow text-xl transition cursor-pointer active:bg-sky-50'>
                     Friday
-                </div>
-                <div className='group p-10 border bg-white hover:border-sky-300 hover:shadow transition cursor-pointer duration-500 active:translate-y-1'>
+                </button>
+                <button type="button" className='group p-10 border bg-white hover:border-sky-300 hover:shadow text-xl transition cursor-pointer active:bg-sky-50'>
                     Saturday
-                </div>
+                </button>
+                {schedule && (
                 <div className='text-xl grid-cols-1'>
-                    <div className='p-2 border-t-2 border-r-2 border-l-2'>9:00 - 10:00</div>
-                    <div className='p-2 border-t-2 border-r-2 border-l-2'>10:00 - 11:00</div>
-                    <div className='p-2 border-t-2 border-r-2 border-l-2'>11:00 - 12:00</div>
-                    <div className='p-2 border-t-2 border-r-2 border-l-2 bg-red-100'>12-13:00 BREAK</div>
-                    <div className='p-2 border-t-2 border-r-2 border-l-2'>13:00 - 14:00</div>
-                    <div className='p-2 border-t-2 border-r-2 border-l-2'>14:00 - 15:00</div>
-                    <div className='p-2 border-t-2 border-r-2 border-l-2 border-b-2'>16:00 - 17:00</div>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>9:00 - 10:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>10:00 - 11:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>11:00 - 12:00</button>
+                    <button type='button' className='w-full p-6 border-t-2 border-r-2 border-l-2 bg-red-100' disabled>12-13:00 BREAK</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>13:00 - 14:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>14:00 - 15:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2 border-b-2'>16:00 - 17:00</button>
                 </div>
+                )}
+                {schedule && (
+                <div className='text-xl grid-cols-1'>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>9:00 - 10:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>10:00 - 11:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>11:00 - 12:00</button>
+                    <button type='button' className='w-full p-6 border-t-2 border-r-2 border-l-2 bg-red-100' disabled>12-13:00 BREAK</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>13:00 - 14:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>14:00 - 15:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2 border-b-2'>16:00 - 17:00</button>
+                </div>
+                )}
+                {schedule && (
+                <div className='text-xl grid-cols-1'>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>9:00 - 10:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>10:00 - 11:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>11:00 - 12:00</button>
+                    <button type='button' className='w-full p-6 border-t-2 border-r-2 border-l-2 bg-red-100' disabled>12-13:00 BREAK</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>13:00 - 14:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>14:00 - 15:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2 border-b-2'>16:00 - 17:00</button>
+                </div>
+                )}
+                {schedule && (
+                <div className='text-xl grid-cols-1'>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>9:00 - 10:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>10:00 - 11:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>11:00 - 12:00</button>
+                    <button type='button' className='w-full p-6 border-t-2 border-r-2 border-l-2 bg-red-100' disabled>12-13:00 BREAK</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>13:00 - 14:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>14:00 - 15:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2 border-b-2'>16:00 - 17:00</button>
+                </div>
+                )}
+                {schedule && (
+                <div className='text-xl grid-cols-1'>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>9:00 - 10:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>10:00 - 11:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>11:00 - 12:00</button>
+                    <button type='button' className='w-full p-6 border-t-2 border-r-2 border-l-2 bg-red-100' disabled>12-13:00 BREAK</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>13:00 - 14:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>14:00 - 15:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2 border-b-2'>16:00 - 17:00</button>
+                </div>
+                )}
+                {schedule && (
+                <div className='text-xl grid-cols-1'>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>9:00 - 10:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>10:00 - 11:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>11:00 - 12:00</button>
+                    <button type='button' className='w-full p-6 border-t-2 border-r-2 border-l-2 bg-red-100' disabled>12-13:00 BREAK</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>13:00 - 14:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2'>14:00 - 15:00</button>
+                    <button type='button' className='w-full hover:shadow hover:bg-gray-50 transition p-6 border-t-2 border-r-2 border-l-2 border-b-2'>16:00 - 17:00</button>
+                </div>
+                )}
             </div>
         </div>
     )
