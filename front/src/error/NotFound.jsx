87f6cf5c7528555181c4423b5ab5789fbe48d499
arm75 <115809877+armql@users.noreply.github.com>
2023-10-13 01:55:24 +0200

    modifying workspace

--- front/src/error/NotFound.jsx
+++ front/src/error/NotFound.jsx
@@ -4,6 +4,7 @@ export default function NotFound() {
     return (
         <div className='h-screen flex justify-center items-center text-4xl font-semibold'>
             Not Found - 404
+            Not found unless you say it is actually found 
         </div>
     )
 }
