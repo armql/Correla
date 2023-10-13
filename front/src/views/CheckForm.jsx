bf5c200cb5132643b944fe3a7309091394255906
arm75 <115809877+armql@users.noreply.github.com>
2023-10-13 13:12:59 +0200

    home changes

--- front/src/views/CheckForm.jsx
+++ front/src/views/CheckForm.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react'
-import cardDoctor from '../assets/svg/doctors.svg'
-import cardFamily from '../assets/svg/family.svg'
+import React, { useState, useEffect } from "react";
+import cardDoctor from "../assets/svg/doctors.svg";
+import cardFamily from "../assets/svg/family.svg";
+import cardHealth from "../assets/svg/health.svg";
 
 export default function CheckForm({ isVisible, onClose }) {
   const [appeared, setAppeared] = useState(false);
@@ -16,59 +17,145 @@ export default function CheckForm({ isVisible, onClose }) {
   }, [isVisible]);
 
   return (
-    <div className='h-full bg-transparent'>
-      <div className='flex justify-between items-center bg-white'>
-        <div className='grid grid-cols-3 gap-2 px-4'>
-          <div className='bg-white border-2 shadow-sm rounded-sm'>
-              <img src={cardFamily} alt="" className='p-4'/>
-              <div className='font-normal bg-gray-50 px-2.5 py-3'>
-                <h1 className='font-semibold border-b-2 border-gray-800 text-lg'>Assured quality</h1>
-                <p className='text-gray-700'>Our crew is proven to be the best</p>
+    <div className="h-full bg-transparent">
+      <div className="flex justify-between items-center bg-white px-6">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 justify-center items-center">
+          <div
+            className="bg-white rounded-sm"
+            style={{ width: "100%", height: "100%" }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+              }}
+            >
+              <img
+                src={cardFamily}
+                alt=""
+                style={{ width: "240px", height: "100px" }}
+              />
+              <div className="font-normal">
+                <h1 className="font-semibold mb-2 border-b-2 border-gray-800 text-lg">
+                  Check Family
+                </h1>
+                <p className="text-gray-700">Always make sure to get checked</p>
               </div>
+            </div>
           </div>
-          <div className='bg-white border-2 shadow-sm rounded-sm'>
-              <img src={cardDoctor} alt="" className='p-4'/>
-              <div className='font-normal bg-gray-50 px-2.5 py-3'>
-                <h1 className='font-semibold border-b-2 border-gray-800 text-lg'>Assured quality</h1>
-                <p className='text-gray-700'>Our crew is proven to be the best</p>
+          <div
+            className="bg-white rounded-sm"
+            style={{ width: "100%", height: "100%" }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+              }}
+            >
+              <img
+                src={cardDoctor}
+                alt=""
+                style={{ width: "100px", height: "100px" }}
+              />
+              <div className="font-normal">
+                <h1 className="font-semibold mb-2 border-b-2 border-gray-800 text-lg">
+                  Assured quality
+                </h1>
+                <p className="text-gray-700">
+                  Our crew is proven to be the best
+                </p>
               </div>
+            </div>
           </div>
-          <div className='bg-white border-2 shadow-sm rounded-sm'>
-              <img src={cardDoctor} alt="" className='p-4'/>
-              <div className='font-normal bg-gray-50 px-2.5 py-3'>
-                <h1 className='font-semibold border-b-2 border-gray-800 text-lg'>Assured quality</h1>
-                <p className='text-gray-700'>Our crew is proven to be the best</p>
+          <div
+            className="bg-white rounded-sm"
+            style={{ width: "100%", height: "100%" }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+              }}
+            >
+              <img
+                src={cardHealth}
+                alt=""
+                style={{ width: "100px", height: "100px" }}
+              />
+              <div className="font-normal">
+                <h1 className="font-semibold mb-2 border-b-2 border-gray-800 text-lg">
+                  Stay healthy!
+                </h1>
+                <p className="text-gray-700">
+                  Our crew is proven to be the best
+                </p>
               </div>
+            </div>
           </div>
         </div>
-        <div className={` p-16 px-14 appear-transition bg-white ${appeared ? 'appeared' : ''}`}>
-      
+
+        <div
+          className={` p-16 px-14 appear-transition bg-white ${
+            appeared ? "appeared" : ""
+          }`}
+        >
           <form action="">
-            <div className='flex items-center justify-center text-xl font-semibold mb-4'>
-              Check form
-            </div>
-            <div className='flex flex-col gap-4'>
-              <div className='flex flex-row gap-2'>
-                <div className='flex flex-col gap-1 font-normal'>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-row gap-2">
+                <div className="flex flex-col gap-1 font-normal">
                   <label htmlFor="firstName">First Name</label>
-                  <input type="text" name="firstName" className='border-2 border-gray-100 rounded-md shadow-sm py-2 px-1.5 font-normal' id="firstName" placeholder='John' />
+                  <input
+                    type="text"
+                    name="firstName"
+                    className="border-2 border-gray-100 rounded-md shadow-sm py-2 px-1.5 font-normal"
+                    id="firstName"
+                    placeholder="Your first name"
+                  />
                 </div>
-                <div className='flex flex-col gap-1 font-normal'>
+                <div className="flex flex-col gap-1 font-normal">
                   <label htmlFor="lastName">Last Name</label>
-                  <input type="text" name="lastName" className='border-2 border-gray-100 rounded-md shadow-sm py-2 px-1.5 font-normal' id="lastName" placeholder='Doe' />
+                  <input
+                    type="text"
+                    name="lastName"
+                    className="border-2 border-gray-100 rounded-md shadow-sm py-2 px-1.5 font-normal"
+                    id="lastName"
+                    placeholder="Your last name"
+                  />
                 </div>
               </div>
-              <div className='flex flex-col gap-2 font-normal'>
-                <label htmlFor="checkDate" className="text-gray-800">Select Check Date</label>
-                <input type="date" name="checkDate" id="checkDate" className='border-2 border-gray-100 rounded-md shadow-sm py-2 px-1.5 font-normal text-gray-400' />
+              <div className="flex flex-col gap-2 font-normal">
+                <label htmlFor="checkDate" className="text-gray-800">
+                  Select Check Date
+                </label>
+                <input
+                  type="date"
+                  name="checkDate"
+                  id="checkDate"
+                  className="border-2 border-gray-100 rounded-md shadow-sm py-2 px-1.5 font-normal text-gray-800"
+                />
               </div>
             </div>
-            <div className='flex justify-center items-center mt-6'>
-              <button type="submit" className='bg-gray-100 px-16 rounded-sm py-3 hover:scale-95 hover:bg-gray-200 transition '>Get Checked</button>
+            <div className="flex justify-center items-center mt-6">
+              <button
+                type="submit"
+                className="bg-emerald-100 text-emerald-900 px-16 rounded-sm py-3 hover:scale-95 hover:bg-emerald-200 transition "
+              >
+                Set Check
+              </button>
             </div>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -1,61 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import { DateTime } from 'luxon';
+import React, { useEffect, useState } from "react";
+import { DateTime } from "luxon";
 
 export default function Home() {
   const initialSchedule = {
     Monday: {
-      '9:00 - 10:00': true,
-      '10:00 - 11:00': false,
-      '11:00 - 12:00': false,
-      '12:00 - 13:00 BREAK': true,
-      '14:00 - 15:00': false,
-      '15:00 - 16:00': false,
-      '16:00 - 17:00': false,
+      "9:00 - 10:00": true,
+      "10:00 - 11:00": false,
+      "11:00 - 12:00": false,
+      "12:00 - 13:00 BREAK": true,
+      "14:00 - 15:00": false,
+      "15:00 - 16:00": false,
+      "16:00 - 17:00": false,
     },
     Tuesday: {
-      '9:00 - 10:00': false,
-      '10:00 - 11:00': false,
-      '11:00 - 12:00': false,
-      '12:00 - 13:00 BREAK': true,
-      '14:00 - 15:00': false,
-      '15:00 - 16:00': false,
-      '16:00 - 17:00': false,
+      "9:00 - 10:00": false,
+      "10:00 - 11:00": false,
+      "11:00 - 12:00": false,
+      "12:00 - 13:00 BREAK": true,
+      "14:00 - 15:00": false,
+      "15:00 - 16:00": false,
+      "16:00 - 17:00": false,
     },
     Wednesday: {
-      '9:00 - 10:00': false,
-      '10:00 - 11:00': false,
-      '11:00 - 12:00': false,
-      '12:00 - 13:00 BREAK': true,
-      '14:00 - 15:00': false,
-      '15:00 - 16:00': false,
-      '16:00 - 17:00': false,
+      "9:00 - 10:00": false,
+      "10:00 - 11:00": false,
+      "11:00 - 12:00": false,
+      "12:00 - 13:00 BREAK": true,
+      "14:00 - 15:00": false,
+      "15:00 - 16:00": false,
+      "16:00 - 17:00": false,
     },
     Thursday: {
-      '9:00 - 10:00': false,
-      '10:00 - 11:00': false,
-      '11:00 - 12:00': false,
-      '12:00 - 13:00 BREAK': true,
-      '14:00 - 15:00': false,
-      '15:00 - 16:00': false,
-      '16:00 - 17:00': false,
+      "9:00 - 10:00": false,
+      "10:00 - 11:00": false,
+      "11:00 - 12:00": false,
+      "12:00 - 13:00 BREAK": true,
+      "14:00 - 15:00": false,
+      "15:00 - 16:00": false,
+      "16:00 - 17:00": false,
     },
     Friday: {
-      '9:00 - 10:00': false,
-      '10:00 - 11:00': false,
-      '11:00 - 12:00': false,
-      '12:00 - 13:00 BREAK': true,
-      '14:00 - 15:00': false,
-      '15:00 - 16:00': false,
-      '16:00 - 17:00': false,
+      "9:00 - 10:00": false,
+      "10:00 - 11:00": false,
+      "11:00 - 12:00": false,
+      "12:00 - 13:00 BREAK": true,
+      "14:00 - 15:00": false,
+      "15:00 - 16:00": false,
+      "16:00 - 17:00": false,
     },
     Saturday: {
-      '9:00 - 10:00': false,
-      '10:00 - 11:00': false,
-      '11:00 - 12:00': false,
-      '12:00 - 13:00 BREAK': true,
-      '14:00 - 15:00': false,
-      '15:00 - 16:00': false,
-      '16:00 - 17:00': false,
+      "9:00 - 10:00": false,
+      "10:00 - 11:00": false,
+      "11:00 - 12:00": false,
+      "12:00 - 13:00 BREAK": true,
+      "14:00 - 15:00": false,
+      "15:00 - 16:00": false,
+      "16:00 - 17:00": false,
     },
   };
 
@@ -65,7 +65,7 @@ export default function Home() {
   const [breakSchedule, setBreakSchedule] = useState({});
 
   const toggleReservation = (day, time) => {
-    if (time.includes('BREAK')) {
+    if (time.includes("BREAK")) {
       setBreakSchedule((prevBreakSchedule) => ({
         ...prevBreakSchedule,
         [day]: {
@@ -85,14 +85,14 @@ export default function Home() {
   };
 
   const isReserved = (day, time) => {
-    if (time.includes('BREAK')) {
+    if (time.includes("BREAK")) {
       return false;
     }
     return schedule[day]?.[time];
   };
 
   const isBreakReserved = (day, time) => {
-    if (time.includes('BREAK')) {
+    if (time.includes("BREAK")) {
       return breakSchedule[day]?.[time];
     }
     return false;
@@ -100,18 +100,21 @@ export default function Home() {
 
   const renderScheduler = (day) => {
     return (
-      <div className='border'>
-        <div className='text-xl grid-cols-1'>
+      <div className="border">
+        <div className="text-xl grid-cols-1">
           {Object.keys(schedule[day]).map((time) => (
             <button
               key={time}
-              type='button'
-              className={`w-full bg-white relative transition text-[18px] p-6 ${(isReserved(day, time) || isBreakReserved(day, time)) ? 'cursor-default bg-white' : ''
-                }`}
+              type="button"
+              className={`w-full bg-white relative transition text-[18px] p-6 ${
+                isReserved(day, time) || isBreakReserved(day, time)
+                  ? "cursor-default bg-white"
+                  : ""
+              }`}
               onClick={() => toggleReservation(day, time)}
             >
               {(isReserved(day, time) || isBreakReserved(day, time)) && (
-                <div className='uppercase absolute text-red-900 bg-red-100 px-1 py-0.5 rounded-tl-sm text-sm right-0 bottom-0'>
+                <div className="uppercase absolute text-red-900 bg-red-100 px-1 py-0.5 rounded-tl-sm text-sm right-0 bottom-0">
                   reserved
                 </div>
               )}
@@ -123,9 +126,8 @@ export default function Home() {
     );
   };
 
-
   const updateCurrentTime = () => {
-    const userTimeZone = 'Europe/Belgrade';
+    const userTimeZone = "Europe/Belgrade";
     const currentTimeInTimeZone = DateTime.now().setZone(userTimeZone);
     setCurrentTime(currentTimeInTimeZone);
   };
@@ -138,31 +140,34 @@ export default function Home() {
     return () => clearInterval(intervalId);
   }, []);
 
-
-
   return (
-    <div className='parent h-full bg-white backdrop-blur-sm bg-opacity-40'>
-      <div className='text-black flex flex-col gap-4 items-center justify-center font-bold'>
+    <div className="parent h-full bg-white backdrop-blur-sm bg-opacity-40">
+      <div className="text-black flex flex-col gap-4 items-center justify-center font-bold">
         <div className={`text-9xl py-6 font-bold`}>
-          {currentTime.toFormat('HH:mm')}
+          {currentTime.toFormat("HH:mm")}
         </div>
       </div>
-      <div className='flex items-center flex-col justify-center'>
-        <div className='grid grid-cols-3 lg:grid-cols-6 items-center justify-center text-center'>
+      <div className="flex items-center flex-col justify-center">
+        <div className="grid grid-cols-3 lg:grid-cols-6 items-center justify-center text-center">
           {Object.keys(schedule).map((day) => (
             <button
               key={day}
-              type='button'
+              type="button"
               className={`bg-white backdrop-blur-sm bg-opacity-70 px-10 py-8 cursor-default uppercase text-lg text-black tracking-tighter font-semibold`}
             >
               {day}
             </button>
           ))}
           {Object.keys(schedule).map((day) => (
-            <div key={day} className=''>
+            <div key={day} className="">
               {renderScheduler(day)}
             </div>
           ))}
+        </div>
+      </div>
+      <div className="bg-white">
+        <div>
+          <h1>Extended full transparency with our customers</h1>
         </div>
       </div>
     </div>
