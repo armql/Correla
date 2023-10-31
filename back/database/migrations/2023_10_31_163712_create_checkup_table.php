afc39d7b7693f0efea86e90682f1c5e5e3fa651d
arm75 <115809877+armql@users.noreply.github.com>
2023-10-31 19:43:31 +0100

    checkup table, model

---
+++ back/database/migrations/2023_10_31_163712_create_checkup_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('checkup', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number', 15);
+            $table->unsignedBigInteger('procedure_id');
+            $table->string('unique_id')->unique();
+            $table->timestamps();
+        });
+
+        // Add foreign key constraint for the procedure_id
+        Schema::table('checkup', function (Blueprint $table) {
+            $table->foreign('procedure_id')->references('id')->on('procedures');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('checkup');
+    }
+};
