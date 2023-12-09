a47cd9b56579e23b5b29336a6b4918e2486997b9
am <arlindmaliqi28@gmail.com>
2023-12-09 21:35:22 +0100

    more assets

--- front/src/assets/svg/arrows/arrows.jsx
+++ front/src/assets/svg/arrows/arrows.jsx
@@ -1,4 +1,6 @@
 import ArrowRight from "./ArrowRight.jsx";
 import ArrowLeft from "./ArrowLeft.jsx";
+import DefArrowRight from "./defArrowRight.jsx";
+import DefArrowLeft from "./defArrowLeft.jsx";
 
-export { ArrowRight, ArrowLeft };
+export { ArrowRight, ArrowLeft, DefArrowLeft, DefArrowRight };
