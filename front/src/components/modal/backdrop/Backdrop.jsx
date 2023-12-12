6d628dac070176e22a19256ebc22733a282927bc
am <arlindmaliqi28@gmail.com>
2023-12-12 14:35:08 +0100

    backdrop changes

--- front/src/components/modal/backdrop/Backdrop.jsx
+++ front/src/components/modal/backdrop/Backdrop.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Backdrop({ children, opacity }) {
   return (
     <div
-      className={`absolute bottom-0 left-0 right-0 top-0 bg-black ${
+      className={`absolute bottom-0 left-0 right-0 top-0 z-10 flex items-center justify-center bg-black ${
         opacity === "0" ? "bg-opacity-0" : "bg-opacity-20"
       }`}
     >
