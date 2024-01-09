b6a3bba36cb0dcde07caceac9c3421f368043015
am <arlindmaliqi28@gmail.com>
2024-01-09 23:56:54 +0100

    lib / core

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -5,12 +5,14 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
 import { ContextProvider } from "./contexts/ContextProvider";
+import App from "./App";
 // import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ContextProvider>
+    <App />
+    {/* <ContextProvider>
       <RouterProvider router={router} />
-    </ContextProvider>
+    </ContextProvider> */}
   </React.StrictMode>,
 );
