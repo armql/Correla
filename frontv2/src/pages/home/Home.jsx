bf3c5dac5ad706575c8487daa0a979c004c5c3df
am <arlindmaliqi28@gmail.com>
2024-01-09 23:56:33 +0100

    home comp w/ asset

---
+++ frontv2/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Background from "../../assets/background.jsx";
+
+export default function Home() {
+  return (
+    <section>
+      Home
+      <div className="flex justify-center items-center">
+        <Background />
+      </div>
+    </section>
+  );
+}
